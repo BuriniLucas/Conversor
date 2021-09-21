bfdc5d94e36314733b8af86eb29e38c9c591e66e
Lucas Burini <burinimarchilucas@gmail.com>
2021-09-21 12:24:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Conversor from './conponents/Conversor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conversor de Moedas </h1>
+      <div className = 'linha'>
+      
+        <Conversor moedaA='USD' moedaB='BRL'></Conversor>
+        <Conversor moedaA='BRL' moedaB='USD'></Conversor>
+      
+      </div>
+      <div className = 'linha'>
+      
+        <Conversor moedaA='CAD' moedaB='BRL'></Conversor>
+        <Conversor moedaA='BRL' moedaB='CAD'></Conversor>
+      
+      </div>
+      <div className = 'linha'>
+      
+        <Conversor moedaA='EUR' moedaB='BRL'></Conversor>
+        <Conversor moedaA='BRL' moedaB='EUR'></Conversor>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
